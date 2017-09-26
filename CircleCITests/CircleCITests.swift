5750aaafe3fb2326856624da19e7ade7b7e1ee5d
administrator <admin@administrators-MacBook-Pro.local>
2017-09-26 17:50:24 +0530

    init

--- CircleCITests/CircleCITests.swift
+++ CircleCITests/CircleCITests.swift
@@ -32,5 +32,10 @@ class CircleCITests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
+    func testMyFunction() {
+        XCTAssert((2 == 2), "false")
+    }
+    func testNewCase() {
+        XCTAssert((2 == 2), "false")
+    }
 }
